8fe8c2c426f124d5d5a8db3b8a1c2e8dbc635387
Aminah Burch <a.j.burch11@gmail.com>
2017-12-28 11:58:40 -0600

    Added doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,30 @@
+class DocsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def doc_params
+    end
+
+    def find_doc
+    end
+end
