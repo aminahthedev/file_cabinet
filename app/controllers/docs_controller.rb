0680dfb3bf7990166e066c35077215f0cea7c32a
Aminah Burch <a.j.burch11@gmail.com>
2017-12-28 12:40:19 -0600

    Added a new, show and index page and updated the controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,8 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
   def index
+    @docs = Doc.all.order("created_at DESC")
   end
 
   def show
@@ -9,6 +12,14 @@ class DocsController < ApplicationController
   end
 
   def create
+    @doc = Doc.new(doc_params)
+
+    if @doc.save
+      redirect_to @doc
+    else
+      flash.now = @doc.errors.full_messages.to_sentence
+      render 'new'
+    end
   end
 
   def edit
@@ -23,8 +34,10 @@ class DocsController < ApplicationController
   private
 
     def doc_params
+      params.require(:doc).permit(:title, :content)
     end
 
     def find_doc
+      @doc = Doc.find(params[:id])
     end
 end
